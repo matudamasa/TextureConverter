686f59f221d1adffb2ddc80818b2cc97b5a2c070
MasatuguMatuda <k021g1292@g.neec.ac.jp>
2023-10-03 15:25:21 +0900

    02_03_exeにファイルを渡す

--- TextureConverter/main.cpp
+++ TextureConverter/main.cpp
@@ -1,8 +1,16 @@
 #include <cstdio>
 #include <cstdlib>
 
-int main() {
-	printf("Hello,World!\n");
+int main(int argc, char* argv[]) {
+
+	// argc‚Ì”‚¾‚¯ŒJ‚è•Ô‚·
+	for (int i = 0; i < argc; i++) {
+		// •¶š—ñargv‚Ì1”Ô‚ğ•\¦
+		printf(argv[i]);
+		// ‰üs
+		printf("\n");
+	
+	}
 
 	system("pause");
 
